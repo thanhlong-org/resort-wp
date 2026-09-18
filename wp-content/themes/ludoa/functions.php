c438b8355e0a92384fc9e2b3ec42e19e96609017
vu-long-duong <duonglongvujvcr@gmail.com>
2026-09-18 14:26:09 +0700

    fix hambur menu

--- wp-content/themes/ludoa/functions.php
+++ wp-content/themes/ludoa/functions.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'LUDOA_VERSION' ) ) {
-	define( 'LUDOA_VERSION', '2.5.5' );
+	define( 'LUDOA_VERSION', '2.6.1' );
 }
 
 // i18n routing + server-side translation, SEO head/sitemap, contact backend.
